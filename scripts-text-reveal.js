d4c13abcc7d539fd16df7ce47189a4ab4277ce82
ekaterinavernigorova <105161186+ekaterinavernigorova@users.noreply.github.com>
2024-01-25 22:19:01 +0300

    new text reveal working without gsap textsplit

--- scripts-text-reveal.js
+++ scripts-text-reveal.js
@@ -1,37 +1,48 @@
-console.clear()
-// GSAP scroll trigger, splittext setup 
-gsap.registerPlugin(SplitText, ScrollTrigger);
-
-const split = new SplitText(".wrapper p", {
-  type: "chars",
-});
-
-const tl  = gsap.timeline({
-  scrollTrigger: {
-    trigger: "#textSection",
-    start: "top top",
-    end: "+=150%",
-    pin: true,
-    scrub: 0.75,
-    markers: false,
+let elementsToSplit = $(".split-lines");
+  let instancesOfSplit = [];
+  // Split the text up
+  function runSplit() {
+    elementsToSplit.each(function (index) {
+      let currentElement = $(this);
+      instancesOfSplit[index] = new SplitType(currentElement, {
+        types: "lines, words"
+      });
+    });
+    $(".line").each(function (index) {
+      $(this).append("<div class='line-mask'></div>");
+    });
   }
-})
-  .set(split.chars, {
-    color: "#ffcc66",
-    stagger: 0.1,
-  }, 0.1);
-
-
-  // Lenis smooth scroll setup 
-const lenis = new Lenis()
-
-lenis.on('scroll', (e) => {
-  console.log(e)
-})
-
-function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-}
-
-requestAnimationFrame(raf)
+  runSplit();
+  // Update on window resize
+  let windowWidth = $(window).innerWidth();
+  window.addEventListener("resize", function () {
+    if (windowWidth !== $(window).innerWidth()) {
+      windowWidth = $(window).innerWidth();
+      elementsToSplit.each(function (index) {
+        instancesOfSplit[index].revert();
+      });
+      runSplit();
+      createAnimation();
+    }
+  });
+  
+  gsap.registerPlugin(ScrollTrigger);
+  
+  function createAnimation() {
+    $(".line").each(function (index) {
+      let tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: $(this),
+          // trigger element - viewport
+          start: "top center",
+          end: "bottom center",
+          scrub: 1
+        }
+      });
+      tl.to($(this).find(".line-mask"), {
+        width: "0%",
+        duration: 1
+      });
+    });
+  }
+  createAnimation();
